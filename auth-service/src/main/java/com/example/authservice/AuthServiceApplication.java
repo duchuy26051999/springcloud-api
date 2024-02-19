1fc5f07fbec1d10984a81148b681dd27a1b22a8d
huynd <huynd26051999@gmail.com>
2024-02-19 17:08:24 +0700

    huynd commit

---
+++ auth-service/src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -0,0 +1,36 @@
+package com.example.authservice;
+
+import com.example.authservice.entity.User;
+import com.example.authservice.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class AuthServiceApplication implements ApplicationRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (userRepository.findByUsername("huynd").isPresent()) {
+            return;
+        }
+        User user = new User();
+        user.setUsername("huynd");
+        user.setPassword(encoder.encode("Unitech@1"));
+        user.setEnable(true);
+        userRepository.save(user);
+    }
+}
